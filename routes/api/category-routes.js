551d4fa3d90f4d7e38d1888f33de4e8ecb70e827
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-29 10:24:56 -0400

    adjusted 'category-routes.js' file to match js syntax of starter code given in 'product-routes.js' file under api folder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,124 +6,110 @@ const { Category, Product } = require('../../models');
 // Category findAll()
 router.get('/', (req, res) => {
   // find all categories
-  try {
-    Category.findAll({
-      // be sure to include its associated Products
-      include: {
-        model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      }
-    })
-    if (!catData) {
-      res.status(404).json({ message: 'Error! No categories found!' });
+Category.findAll({
+    // be sure to include its associated Products
+  include: {
+    model: Product,
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+  }
+})
+  .then(catData => {
+    if(!dbCatData) {
+      res.status(404).json({message: 'Error! No categories found!'});
       return;
     }
     res.json(catData);
-  }
-  catch (err) {
+  })
+  .catch(err => {
     console.log(err);
     res.status(500).json(err)
-  }
+  });
 });
-
-
 
 
 // Category findOne()
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  try {
-    Category.findOne({
-      where: {
-        id: req.params.id
-      },
-      // be sure to include its associated Products
-      include: {
-        model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      }
-    })
-    if (!catData) {
-      res.status(404).json({ message: 'Error! No categories found!' });
+Category.findOne({
+  where: {
+    id: req.params.id
+  },
+  // be sure to include its associated Products
+  include: {
+    model: Product,
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+  }
+})
+  .then(catData => {
+    if(!catData) {
+      res.status(404).json({message: 'Error! No categories found!'});
       return;
     }
     res.json(catData);
-  }
-  catch (err) {
+  })
+  .catch(err => {
     console.log(err);
     res.status(500).json(err)
-  }
+  });
 });
-
-
 
 
 // Category POST 
 router.post('/', (req, res) => {
   // create a new category
-  try {
-    Category.create({
-      where: {
-        category_name: req.body.category_name,
-      }
-    })
-    res.status(200).json(catData);
-  }
-  catch (err) {
-    res.status(400).json(err);
-  }
+Category.create({
+  category_name: req.body.category_name
+})
+  .then(catData => res.json(catData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
-
-
 
 
 // Category PUT 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  try {
-    Category.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
-    if (!catData) {
-      res.status(404).json({ message: 'Sorry, no category found with this id!' });
+Category.update(req.body, {
+  where: {
+    id: req.params.id
+  }
+})
+  .then(dbCatData => {
+    if (!dbCatData) {
+      res.status(404).json({message:'Sorry, no category found with this id!'});
       return;
     }
-    res.json(catData);
-  }
-  catch (err) {
+    res.json(dbCatData);
+  })
+  .catch(err => {
     console.log(err);
-    res.status(500).json(err)
-  }
+    res.status(500).json(err);
+  });
 });
-
-
-
 
 
 // Category DELETE
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  try {
-    Category.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    if (!catData){
+Category.destroy({
+  where: {
+    id: req.params.id
+  }
+})
+  .then(dbCatData => {
+    if (!dbCatData){
       res.status(404).json({message: 'Sorry, no category found with that id!'});
       return;
     }
-    res.json(catData);
-  }
-  catch (err) {
+    res.json(dbCatData);
+  })
+  .catch(err => {
     console.log(err);
-    res.status(500).json(err)
-  }
+    res.status(500).json(err);
+  });
 });
-
-
 
 
 // Export router module
